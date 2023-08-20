716b2666b68ddee908108e448713fa9e15328a79
wbatek <wojciech.batko02@gmail.com>
2023-08-20 17:39:42 +0200

    house solution

--- python/house/house.py
+++ python/house/house.py
@@ -1,2 +1,22 @@
+VERSES = [
+    'the house that Jack built.',
+    'the malt that lay in ',
+    'the rat that ate ',
+    'the cat that killed ',
+    'the dog that worried ',
+    'the cow with the crumpled horn that tossed ',
+    'the maiden all forlorn that milked ',
+    'the man all tattered and torn that kissed ',
+    'the priest all shaven and shorn that married ',
+    'the rooster that crowed in the morn that woke ',
+    'the farmer sowing his corn that kept ',
+    'the horse and the hound and the horn that belonged to '
+]
+
+
+def verse(n):
+    return 'This is ' + ''.join([VERSES[i] for i in range(n - 1, -1, -1)])
+
+
 def recite(start_verse, end_verse):
-    pass
+    return [verse(i) for i in range(start_verse, end_verse + 1)]
